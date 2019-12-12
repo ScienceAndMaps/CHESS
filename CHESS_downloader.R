01056b13f4d4e254cbff9c5100170ad763d3bac1
Philip Taylor <philor@ceh.ac.uk>
2019-12-12 11:16:04 +0000

    Create CHESS_downloader.R

---
+++ CHESS_downloader.R
@@ -0,0 +1,34 @@
+# CHESS data: https://eip.ceh.ac.uk/chess
+
+require("filesstrings")
+
+vars <- c("precip","peti") # choose from: dtr, huss, pet, peti, precip, psurf, rlds, sfcWind, tas
+years <- 1986:2005 # choose range
+months <- c("01","02","03","04","05","06","07","08","09","10","11","12") # choose months
+
+wd <- paste0(dirname(rstudioapi::getSourceEditorContext()$path),"/") # if not running in RStudio, set wd to directory of this script, e.g. wd <- 'C:/Dir/'
+
+for (var in vars) {
+  
+  for (year in years) {
+
+    for (month in months) {
+  
+      if (var=="pet" | var=="peti") {
+        file <- paste0("chess_",var,"_wwg_",year,month,".nc")
+        url <- paste0("https://eip.ceh.ac.uk/thredds/fileServer/",toupper(var),"Detail02/",file)
+      } else {
+        file <- paste0("chess_",var,"_",year,month,".nc")
+        url <- paste0("https://eip.ceh.ac.uk/thredds/fileServer/public-CHESSDrivingData",var,"Detail02/",file)
+      }
+      path1 <- tempfile(fileext = ".nc") # create temp file
+      download.file(url, path1, mode="wb") # download
+      file.rename(path1,paste0(dirname(path1),"/",file)) # rename
+      newpath <- paste0(dirname(path1),"/",file) # path to wd / file
+      file.move(newpath, paste0(wd,"GIS_Data/CHESS/")) # move to path
+  
+    }
+  
+  }
+
+}
